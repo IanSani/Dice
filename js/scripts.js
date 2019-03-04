6533535e93a1d2c5aebd4f00be4309e22add54ea
IanSani <iansani259@gmail.com>
2019-03-04 14:17:48 +0300

    Added function start game to initialize game start

--- js/scripts.js
+++ js/scripts.js
@@ -31,11 +31,11 @@ Player.prototype.hold= function(){
 }
 // check for 50!
 player.prototype.winnerCheck=function(){
-  if(this.totalscore>=50){
+  if(this.totalscore>=100){
     alert(this.playerName + " You won!"")
   }
 }
-player.prototype.newGame= function(){
+player.prototype.startGame= function(){
   this.roll=0;
   this.tempscore=0;
   this.totalscore=0;
@@ -67,12 +67,12 @@ $(document).ready(function(){
  $("button#start-game").click(function(event){
    $(".player-console").hide();
    clearValues();
-   player1.newGame();
-   player2.newGame();
+   player1.startGame();
+   player2.startGame();
    $("#session-total-1").empty();
    $("#total-score-1").empty();
    $("#die-roll-1").empty();
-   $("#round-total-2").empty();
+   $("#session-total-2").empty();
    $("#total-score-2").empty();
    $("#die-roll-2").empty();
 
@@ -82,26 +82,26 @@ $(document).ready(function(){
      player1.roll = throwdice();
      $("#die-roll-1").text(player1.roll);
      player1.rollone();
-     $("#round-total-1").text(player1.tempscore);
+     $("#session-total-1").text(player1.tempscore);
    });
 
    $("button#player2-roll").click(function(event){
      player2.roll = throwdice();
      $("#die-roll-2").text(player2.roll);
      player2.rollone();
-     $("#round-total-2").text(player2.tempscore);
+     $("#session-total-2").text(player2.tempscore);
    });
    $("button#player1-hold").click(function(event){
     player1.hold();
     $("#total-score-1").text(player1.totalscore);
-    $("#round-total-1").empty();
+    $("#session-total-1").empty();
     $("#die-roll-1").empty();
     player1.winnerCheck();
   });
   $("button#player2-hold").click(function(event){
     player2.hold();
     $("#total-score-2").text(player2.totalscore);
-    $("#round-total-2").empty();
+    $("#session-total-2").empty();
     $("#die-roll-2").empty();
     player2.winnerCheck();
   });
