05f97d0c7efffe790e169a325f9d2d117d14c7d3
IanSani <iansani259@gmail.com>
2019-03-04 12:00:59 +0300

    Added hold function for player 2 when dice is hold

--- js/scripts.js
+++ js/scripts.js
@@ -98,3 +98,10 @@ $(document).ready(function(){
     $("#die-roll-1").empty();
     player1.winnerCheck();
   });
+  $("button#player2-hold").click(function(event){
+    player2.hold();
+    $("#total-score-2").text(player2.totalscore);
+    $("#round-total-2").empty();
+    $("#die-roll-2").empty();
+    player2.winnerCheck();
+  });
