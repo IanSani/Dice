b5e07d71ce0decc5f53f44a5013f751dc37bdd1f
IanSani <iansani259@gmail.com>
2019-03-04 11:21:52 +0300

    Added function for rollone with condition when one is rolled

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
+// Business logic
+var player1="";
+var player2="";
 
-var iRandomNumber;
-var iRollDice;
-iRollDice=
-iRandomNumber= Math.ceil(Math.random()*6);
+var throwdice= function (){
+  return Math.floor(6*Math.random())+1;
+}
 
-document.writeln(iRandomNumber);
+function player(turn){
+  this.roll=0;
+  this.tempscore=0;
+  this.totalscore=0;
+  this.turn=turn;
+  this.playerName;
+}
+//checking for player 1
+player.prototype.rollone=function(){
+  if(this.roll===1){
+    this.tempscore=0;
+    alert("Oooops"+ this.playerName +", you rolled a 1! Next Player")
+  }
+  else{
+    this.tempscore+= this.roll;
+  }
+}
